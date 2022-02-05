4a47e1dd7f08b5d6d45dc813fec3998cb708e3d0
Ben-Garcia-Data <ben@garciafamily.co.uk>
2022-02-05 23:22:40 +0000

    Does not build
    I re-created all the DMX fixtures so they were all much neater and then attempted to create a new class for an 8 colour mix. This caused the SainsburyTheatreVis Module to fail to build. I restarted the engine and now cannot get the file to load. Shit. Commiting now so that if I make more mistakes I can go back to here.

---
+++ Source/SainsburyTheatreVis/DMXFixture8ComponentColor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DMXFixtureComponentColor.h"
+#include "DMXFixture8ComponentColor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAINSBURYTHEATREVIS_API UDMXFixture8ComponentColor : public UDMXFixtureComponentColor
+{
+	GENERATED_BODY()
+	
+};
