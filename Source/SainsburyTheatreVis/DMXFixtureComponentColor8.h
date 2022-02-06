aa1395c7bb938b5b718175c8da89aa60f867b48c
Ben-Garcia-Data <ben@garciafamily.co.uk>
2022-02-06 02:43:10 +0000

    Refocus rig and fix DMX issues
    A lot of work in this commit.

---
+++ Source/SainsburyTheatreVis/DMXFixtureComponentColor8.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DMXFixtureComponentColor.h"
+#include "DMXFixtureComponentColor8.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAINSBURYTHEATREVIS_API UDMXFixtureComponentColor8 : public UDMXFixtureComponentColor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SainsburyTheatreVis/DMXFixtureComponentColors8.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DMXFixtureComponentColor.h"
+#include "DMXFixtureComponentColors8.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAINSBURYTHEATREVIS_API UDMXFixtureComponentColors8 : public UDMXFixtureComponentColor
+{
+	GENERATED_BODY()
+	
+};
